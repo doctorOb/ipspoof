a0ec48c340ab6629b70515bc388bf486d26afb37
Dan Robertson <drobertson@kayak.com>
2014-02-16 20:56:34 -0500

    wrote simple shell script to merge our modified ns-3 files into an instilation of the full framework

---
+++ code/apply_patch.sh
@@ -0,0 +1,8 @@
+echo $1
+cp -v ns-3-mod/global-router-interface.* $1/ns-3.19/src/internet/model
+cp -v ns-3-mod/ipv4-routing-table-entry.* $1/ns-3.19/src/internet/model
+cp -v ns-3-mod/ipv4-global-routing.* $1/ns-3.19/src/internet/model
+cp -v ns-3-mod/ipv4-header.* $1/ns-3.19/src/internet/model
+cp -v ns-3-mod/second.cc $1/ns-3.19/examples/tutorial
+
+
